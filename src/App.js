b75b72aa593b86e5b46af1f72ec86103e7f0d2b4
leodaiub <leodaiub@gmail.com>
2019-03-25 22:07:31 -0300

    created components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from  './components/Navigation/Navigation.js'
-import Logo from  './components/Logo/Logo.js'
+import Navigation from  './components/Navigation/Navigation.js';
+import Logo from  './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from  './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
+const particlesOptions ={
+  particles: {
+      number: {
+          value: 250,
+          density: {
+            enable: true,
+            value_area: 1000
+       }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles  className='particles'  params={particlesOptions}/>
         <Navigation/>
         <Logo/>
-        {/*<ImageLinkForm/>
-        <FaceRecognition/>*/}
+        <Rank/>
+        <ImageLinkForm/>
+        {/*<FaceRecognition/>*/}
       </div>
     );
   }
