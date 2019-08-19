9b28877f60d8c366dd0b2da1e3da02b626d21971
Leonardo Daiub <45826152+leodaiub@users.noreply.github.com>
2019-08-19 17:24:42 -0300

    Update SignIn.js

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3030/signin', {
+        fetch('https://smart-brain-leodaiub.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
